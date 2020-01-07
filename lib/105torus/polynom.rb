f2d5c240996392fe36638ec1954ea8ddd31e997e
Tanguy Andreani <tanguy.andreani@epitech.eu>
2020-01-07 15:33:39 +0100

    typo

--- lib/105torus/polynom.rb
+++ lib/105torus/polynom.rb
@@ -10,15 +10,15 @@ class Polynom
             r + (v*(x**i))
         }
     end
-    def bissection(s: 0.0, e: 1.0, p: 6, &block)
+    def bisection(s: 0.0, e: 1.0, p: 6, &block)
         case @degree
         when 4
             if (calculate(e) * calculate(s) <= 0)
                 mid = (s + e)/2.0
                 return if ((s - e).abs / (2 * mid.abs)) < 0.1**p
                 yield(mid)
-                bissection(s: s, e: mid, p: p, &block)
-                bissection(s: mid, e: e, p: p, &block)
+                bisection(s: s, e: mid, p: p, &block)
+                bisection(s: mid, e: e, p: p, &block)
             end
         end
     end
